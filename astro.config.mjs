97bf4610c0213fdd5ca2745b9d71ccd209716549
Patrick Connelly <patrick@deadlypenguin.com>
2024-04-08 09:21:26 -0400

    Adding sitemap and robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
 import { defineConfig } from 'astro/config';
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
-	output: 'static',
-	build: {
-		assets: 'astro'
-	}
+  output: 'static',
+  site: 'https://connelly.dev',
+  build: {
+    assets: 'astro'
+  },
+  integrations: [sitemap()]
 });
